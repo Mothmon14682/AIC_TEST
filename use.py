af96571f916bc72abb5ee162832d977cb696b52d
Mothmon14682 <huynhanhtuan14686@gmail.com>
2025-08-10 16:56:44 +0700

    remove somethings

--- use.py
+++ use.py
@@ -1,7 +1,7 @@
 import joblib
 import torch
 from transformers import CLIPProcessor, CLIPModel
-from utils import create_faiss_index, extract_text_vector, extract_image_vector, show_unranked_frames, similarity_text_with_video, rerank_with_nosac, show_reranked_frames
+from utils import create_faiss_index, extract_text_vector, extract_image_vector, show_unranked_frames, similarity_text_with_video 
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16")
@@ -10,12 +10,14 @@ model.to(device)
 
 features = joblib.load("features.pkl")
 
-def image_retrival():
+def image_retrival(folder):
     while True:
         img = input("a pic in test folder: ")
-        image_vector = extract_image_vector(f"test/{img}.jpg", model, processor, device)
+        image_vector = extract_image_vector(f"{folder}/{img}.jpg", model, processor, device)
         index = create_faiss_index(features)
         top_sim = similarity_text_with_video(features, image_vector, index, 10) 
+        
+        show_unranked_frames(top_sim)
 
         print("------Before reranked------")
         i = 1
@@ -24,18 +26,6 @@ def image_retrival():
                 break 
             print(f"{frame}: score={score}")
             i += 1
-
-        i = 1
-        print("---------Reranked----------")
-        reranked = rerank_with_nosac(top_sim)
-        for frame, score, inliers in reranked:
-            if i > 5:
-                break
-            print(f"{frame}: score={score:.4f}, inliers={inliers}")
-            i += 1
-
-        show_unranked_frames(top_sim)
-        show_reranked_frames(reranked, top_n = 5)
 
 def text_retrival():
     while True:
@@ -43,6 +33,8 @@ def text_retrival():
         text_vector = extract_text_vector(prompt, model, processor, device)
         index = create_faiss_index(features)
         top_sim = similarity_text_with_video(features, text_vector, index, 10) 
+        
+        show_unranked_frames(top_sim)
 
         print("------Before reranked------")
         i = 1
@@ -51,17 +43,5 @@ def text_retrival():
                 break 
             print(f"{frame}: score={score}")
             i += 1
-
-        i = 1
-        print("---------Reranked----------")
-        reranked = rerank_with_nosac(top_sim)
-        for frame, score, inliers in reranked:
-            if i > 5:
-                break
-            print(f"{frame}: score={score:.4f}, inliers={inliers}")
-            i += 1
-
-        show_unranked_frames(top_sim)
-        show_reranked_frames(reranked, top_n = 5)
 
 text_retrival()
